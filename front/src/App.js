c20059adcbd3b7e082d533111686a0a343204547
Andre Sbrocco Figueiredo <andresbrocco@gmail.com>
2021-09-22 12:46:47 +0200

    filter by gender works 99%

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
 import MoviesList from "./Components/MoviesList";
-import GenresList from "./Components/GenresList";
-import {useState} from "react";
 
 function App() {
-    const [selectedGenres, setSelectedGenres] = useState([]);
-
     return (
-    <div className="App">
-        <MoviesList selectedGenres={selectedGenres}/>
-        <GenresList selectedGenres={selectedGenres} setSelectedGenres={setSelectedGenres}/>
-    </div>
+        <div className="App">
+            <MoviesList/>
+        </div>
     );
 }
 
--- front/src/Components/MoviesList.js
+++ front/src/Components/MoviesList.js
@@ -1,35 +1,59 @@
 import MovieItem from "./MovieItem";
-import {useEffect, useState} from "react";
+import {Fragment, useEffect, useState} from "react";
 import API from "../API/API";
 
-export default function MoviesList({selectedGenres}){
+export default function MoviesList(){
     const [movies, setMovies] = useState([]);
-    const [fisrt, setFisrt] = useState(true);
+    const [genres, setGenres] = useState([]);
+    const [selectedGenres, setSelectedGenres] = useState([]);
+
     useEffect(() => {
         API.filmsPlusRecents().then(data => {
-
             setMovies(data.hits.hits)
-            setFisrt(false)
         });
+        API.allGenres().then(data => {
+            setGenres(data.aggregations.genres.buckets)
+            setSelectedGenres([])
+        })
     }, [])
 
-    useEffect(() => {
-        console.log(fisrt)
-        if (!fisrt) {
-            console.log(fisrt)
-            API.byGenre(selectedGenres).then(data => {
-                setMovies(data.hits.hits)
-            })
-        }
-    }, [selectedGenres])
+    const toggleGenre = (genre) => () => {
+        let selectedGenres_ = selectedGenres.includes(genre) ? selectedGenres.filter(g => g !== genre) : [...selectedGenres, genre]
+        API.byGenre(selectedGenres_).then(data => {
+            setMovies(data.hits.hits)
+            setSelectedGenres(selectedGenres_)
+        })
+    }
 
     const showMovies = () => {
-        return movies.map((movie) => (
-        <MovieItem
-            key={movie._source.id}
-            movie={movie._source}
-        />
-        ))}
+        if (movies.length === 0) {
+            return <></>
+        } else {
+            return movies.map((movie) => (
+                <MovieItem
+                    key={movie._source.id}
+                    movie={movie._source}
+                />
+            ))
+        }
+    }
+
+    const showGenres = () => {
+        if (genres.length === 0) {
+            return <></>
+        } else {
+            return genres.map((genre) => (
+                <Fragment key={genre.key}>
+                    <label>
+                        {genre.key} ({genre.doc_count})
+                        <input type="checkbox" id={genre.key} name={genre.key} value={genre.key}
+                               onChange={toggleGenre(genre.key)}/>
+                    </label>
+                    <br/>
+                </Fragment>
+            ))
+        }
+    }
 
     return (
         <>
@@ -37,6 +61,11 @@ export default function MoviesList({selectedGenres}){
             <ul>
                 {showMovies()}
             </ul>
+            <h2>Genres List:</h2>
+            <form >
+                {showGenres()}
+                <br/>
+            </form>
         </>
     )
 }
