e67a74f3f4f1150e4d6d3c93d9f25575f019b6a6
Maxime Daude <maximedaude.dev@gmail.com>
2021-09-23 16:15:35 +0200

    Modif MovieItem + Order Date boutton

--- front/src/Components/MovieItem.js
+++ front/src/Components/MovieItem.js
@@ -1,11 +1,44 @@
 export default function MovieItem({movie}){
+
+    const styles = {
+        backgroundColor: "#bbbbbb",
+        color:"#000000",
+        listStyle: "none"
+    }
+
+    const secondToDate = (seconds) => {
+        const date = new Date(seconds);
+        return date.toString()
+    }
+
+    const showGenres = (genres) => {
+        if (genres.length === 0) {
+            return <></>
+        } else {
+            return genres.map( (genre) =>
+                <li key={genre} style={{listStyle: "none"}}>
+                    {genre}
+                </li>
+            )
+        }
+    }
+
+    /*
+
+    */
     return(
-        <li>
+        <li style={styles}>
             <p>{movie.title}</p>
             <p>{movie.overview}</p>
-            <p>{movie.genres}</p>
-            <p>{movie.release_date}</p>
-            <p>{movie.poster}</p>
+            <ul>
+                {showGenres(movie.genres)}
+            </ul>
+            <p>{secondToDate(movie.release_date)}</p>
+            <img
+                alt={"poster"}
+                src={movie.poster}
+                style={{width:100, height:100}}
+            />
         </li>
     )
 }
